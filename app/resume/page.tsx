ab10f2a39fbccaf6bf25264936097c2e5ff8e8b9
Mike <mikemcguiness@protonmail.com>
2023-07-10 20:49:16 -0700

    update resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -4,7 +4,7 @@ export default function Resume() {
   return (
     <main className="">
       <div className='w-full px-2 md:px-0 md:w-5/6 lg:w-3/4 mx-auto mt-12'>
-        <div className='pt-6'>
+        <div className='py-6'>
           <h4 className='font-bold text-xl'>
             Michael McGuiness
           </h4>
@@ -23,10 +23,10 @@ export default function Resume() {
           </div>
         </div>
 
-        <div className="divider my-0 before:bg-black after:bg-black dark:before:bg-slate-400 dark:after:bg-slate-400 mt-4"></div>
+        <div className="divider my-0 before:bg-black after:bg-black dark:before:bg-slate-400 dark:after:bg-slate-400"></div>
         <div className="divider my-0 before:bg-green-600 after:bg-green-600"></div>
 
-        <div className='pt-6'>
+        <div className='py-6'>
           <h2 className='font-bold'>
             PROFILE SUMMARY
           </h2>
@@ -35,35 +35,35 @@ export default function Resume() {
           </p>
         </div>
 
-        <div className="divider my-0 before:bg-black after:bg-black mt-4 dark:before:bg-slate-400 dark:after:bg-slate-400"></div>
+        <div className="divider my-0 before:bg-black after:bg-black dark:before:bg-slate-400 dark:after:bg-slate-400"></div>
         <div className="divider my-0 before:bg-green-600 after:bg-green-600"></div>
 
 
-        <div className='pt-6'>
+        <div className='py-6'>
           <h2 className='font-bold'>
             TECHNICAL SKILLS
           </h2>
-          <ul className='flex flex-col gap-y-2 list-disc ps-8 pt-4'>
-            <li><span className='font-bold'>Front-End Development:</span> React.js, Next.js, Tailwind CSS, HTML, CSS, JavaScript, Jest, Git</li>
+          <ul className='flex flex-col gap-y-2 pt-4'>
+            <li><span className='font-bold'>Front-End:</span> HTML, CSS, JavaScript, Typescript, React, Next.js, Tailwind CSS, Jest, Git</li>
             <li><span className='font-bold'>Database Management:</span> Prisma, Supabase, Firebase</li>
             <li><span className='font-bold'>Cloud Services:</span> Google Cloud Storage, Google Cloud Run</li>
             <li><span className='font-bold'>APIs:</span> OpenAI API, Google Maps API, Yelp API</li>
-            <li><span className='font-bold'>Others:</span> Unity, C#, Blender, Substance Painter, Audacity</li>
+            <li><span className='font-bold'>Others:</span> Python, Unity, C#, Blender</li>
           </ul>
         </div>
 
-        <div className="divider my-0 before:bg-black after:bg-black mt-4 dark:before:bg-slate-400 dark:after:bg-slate-400"></div>
+        <div className="divider my-0 before:bg-black after:bg-black dark:before:bg-slate-400 dark:after:bg-slate-400"></div>
         <div className="divider my-0 before:bg-green-600 after:bg-green-600"></div>
 
 
-        <div className='pt-6'>
+        <div className='py-6'>
           <h2 className='font-bold'>
             EDUCATION
           </h2>
           <div className="flex flex-col gap-y-2 pt-4">
             <div className='flex'>
               <p>
-                Bachelor of Science in Biotechnology
+                Bachelor of Science in Mathematics, Cum Laude
               </p>
               <div className="divider divider-horizontal"></div>
               <p>
@@ -77,7 +77,7 @@ export default function Resume() {
             </div>
             <div className='flex'>
               <p>
-                Bachelor of Science in Mathematics
+                Bachelor of Science in Biotechnology, Cum Laude
               </p>
               <div className="divider divider-horizontal"></div>
               <p>
@@ -92,11 +92,11 @@ export default function Resume() {
           </div>
         </div>
 
-        <div className="divider my-0 before:bg-black after:bg-black mt-4 dark:before:bg-slate-400 dark:after:bg-slate-400"></div>
+        <div className="divider my-0 before:bg-black after:bg-black dark:before:bg-slate-400 dark:after:bg-slate-400"></div>
         <div className="divider my-0 before:bg-green-600 after:bg-green-600"></div>
 
 
-        <div className='pt-6'>
+        <div className='py-6'>
           <h2 className='font-bold'>
             WORK EXPERIENCE
           </h2>
@@ -120,41 +120,44 @@ export default function Resume() {
           </ul>
         </div>
 
-        <div className="divider my-0 before:bg-black after:bg-black mt-4 dark:before:bg-slate-400 dark:after:bg-slate-400"></div>
+        <div className="divider my-0 before:bg-black after:bg-black dark:before:bg-slate-400 dark:after:bg-slate-400"></div>
         <div className="divider my-0 before:bg-green-600 after:bg-green-600"></div>
 
 
-        <div className='pt-6'>
-          <h2 className='font-bold'>
+        <div className='py-6'>
+          <h2 className='font-bold pb-4'>
             PROJECTS
           </h2>
-          <div className='pt-4'>
-            <h4 className='font-bold'>
+          <div className='pb-4'>
+            <h4 className='font-bold pb-2'>
               Service Now Auto Repair
             </h4>
-            <ul className='list-disc ps-8 py-2'>
+            <ul className='flex flex-col gap-y-2 list-disc ps-8'>
               <li>Developed a responsive web application with real-time review data and integrated Google Maps</li>
+              <li>Project Link: <a href="https://service-now-auto-repair.vercel.app/" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600' >service-now-auto-repair.vercel.app</a></li>
             </ul>
           </div>
-          <div>
-            <h4 className='font-bold'>
+          <div className='pb-4'>
+            <h4 className='font-bold pb-2'>
               Journal Transcribe
             </h4>
-            <ul className='list-disc ps-8 py-2'>
+            <ul className='flex flex-col gap-y-2 list-disc ps-8'>
               <li>Designed a system to process, index, and store images of journal pages, featuring user input, zoom functionality, and speech-to-text capabilities</li>
+              <li>Project Link: <a href="https://mike-v2.github.io/journal-transcribe/" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>mike-v2.github.io/journal-transcribe</a></li>
             </ul>
           </div>
-          <div>
-            <h4 className='font-bold'>
+          <div className=''>
+            <h4 className='font-bold pb-2'>
               Harry Howard&apos;s Journals
             </h4>
-            <ul className='list-disc ps-8 py-2'>
+            <ul className='flex flex-col gap-y-2 list-disc ps-8'>
               <li>Created a user-focused platform for browsing and searching journal entries, with state-of-the-art AI that enables users to explore a large dataset with natural language</li>
+              <li>Project Link: <a href="https://journal-search.vercel.app/" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>journal-search.vercel.app</a></li>
             </ul>
           </div>
         </div>
 
-        <div className="divider my-0 before:bg-black after:bg-black mt-4 dark:before:bg-slate-400 dark:after:bg-slate-400"></div>
+        <div className="divider my-0 before:bg-black after:bg-black dark:before:bg-slate-400 dark:after:bg-slate-400"></div>
         <div className="divider my-0 before:bg-green-600 after:bg-green-600"></div>
 
 
