da08bab57086c16585810ef7d8172272744eda22
Mike <mikemcguiness@protonmail.com>
2023-09-06 11:44:08 -0700

    fix unescaped characters

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -182,8 +182,8 @@ export default function Resume() {
             RESEARCH CONTRIBUTIONS
           </h2>
           <ul className='flex flex-col gap-y-2 pt-4'>
-            <li>Rana V Smalling, Don A Delker, Yuxia Zhang, Natalia Nieto, <span className='font-bold'>Michael S McGuiness</span>, Shuanghu Liu, Scott L Friedman, Curt H Hagedorn, Li Wang. "Genome-wide transcriptome analysis identifies novel gene signatures implicated in human
-              chronic liver disease" - <span className='italic'>American Journal of Physiology - Gastrointestinal and Liver Physiology</span>, 2013.</li>
+            <li>Rana V Smalling, Don A Delker, Yuxia Zhang, Natalia Nieto, <span className='font-bold'>Michael S McGuiness</span>, Shuanghu Liu, Scott L Friedman, Curt H Hagedorn, Li Wang. &quot;Genome-wide transcriptome analysis identifies novel gene signatures implicated in human
+              chronic liver disease&quot; - <span className='italic'>American Journal of Physiology - Gastrointestinal and Liver Physiology</span>, 2013.</li>
           </ul>
         </div>
       </div>
