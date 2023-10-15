ba5b6638456fdb0341af6be128f466785323ccad
Mike <mikemcguiness@protonmail.com>
2023-10-14 18:08:52 -0700

    remove elastic search and lunr search

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,7 +9,7 @@ const projectData = [
     summaryHeader: 'Using AI to Make Big Data More Accessible',
     summary: "I'm proud to introduce Harry Howard's Journals, an advanced web application that redefines the way we explore historical journal entries. Using Next.js, Python, and AI tools, the app processes and analyzes entries from the 1930s and 40s. These entries are enhanced by AI and made easily accessible, putting a treasure trove of historical data at your fingertips.",
     features: ["Harry has been brought to life with state-of-the-art AI technology. Ask Harry detailed questions about the journals, or just chat! (powered by ChatGPT)", "Read journal entries one page at a time or easily skip around", "Search function for journal entries", "AI-generated topics for each entry allow users to understand an entry at a glance and link together stories woven through multiple entries", "Sign in with Google to keep track of pages read, save special passages and make posts to discuss entries with others"],
-    primaryTools: ["React", "Next.js", "Node.js", "TypeScript", "Tailwind CSS", "Daisy UI", "ChatGPT API", "Prisma", "Supabase", "ElasticSearch", "Lunr Search"],
+    primaryTools: ["React", "Next.js", "Node.js", "TypeScript", "Tailwind CSS", "Daisy UI", "ChatGPT API", "Prisma", "Supabase"],
     secondaryTools: ["Use Python library OpenCV to preprocess images", "Run CRAFT AI locally to split images into lines", "Apply Google Vision API on lines", "Use GPT-4 to reconstruct journal entry from Google Vision output", "Generate metadata on journal entries with GPT-3.5 (ChatGPT) API", "Store data with Google Cloud Storage", "Run python scripts with Google Cloud Run"]
   },
   {
