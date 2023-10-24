fdc48378c2d2a523f25394ab02fe17fec966b478
Mike <mikemcguiness@protonmail.com>
2023-10-24 14:20:14 -0700

    add video and article links

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,6 +5,8 @@ const projectData = [
     imagePath: '/images/recreating-react-logo.png',
     title: 'Recreating React',
     subtitle: 'Building Four Core Features of React',
+    article: 'https://medium.com/@mmcguiness8700/recreating-react-96e8a01d4cb5',
+    video: 'https://youtu.be/BU37lgD28lU',
     source: 'https://github.com/mike-v2/react-from-scratch',
     summary: <span>In this self-driven project, I undertook the challenge of recreating a simplified version of React from scratch to deepen my understanding of its inner workings.By building out core functions like createElement, render, useState, and useEffect, I gained valuable insights into JSX transformation, DOM manipulation, and state management.This hands-on experience has not only solidified my grasp of React fundamentals but also honed my problem-solving skills.I&apos;ve documented my journey and the technical intricacies of the project in a <a target='blank' href="https://medium.com/@mmcguiness8700/recreating-react-96e8a01d4cb5" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>Medium article</a> and a <a target='blank' href="https://youtu.be/BU37lgD28lU" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>YouTube video</a> for those interested in diving deeper.</span>,
     features: [<p key={0}><span className='font-bold'>createElement</span> transforms JSX into ReactElement objects</p>, <p key={1}><span className='font-bold'>render</span> brings ReactElements to life on the DOM</p>, <p key={2}><span className='font-bold'>useState</span> manages state</p>, <p key={3}><span className='font-bold'>useEffect</span> synchronizes the component with an external system</p>],
@@ -106,6 +108,16 @@ export default function Projects() {
                       {project.link &&
                         <a href={project.link} target='_blank' className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600 pr-5'>
                           {linkText}
+                        </a>
+                      }
+                      {project.article &&
+                        <a href={project.article} target='_blank' className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600 pr-5'>
+                          Article
+                        </a>
+                      }
+                      {project.video &&
+                        <a href={project.video} target='_blank' className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600 pr-5'>
+                          Video
                         </a>
                       }
                       <a href={project.source} target='_blank' className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>
