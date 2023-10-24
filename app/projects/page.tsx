5de8fc074a597943ba6d62679d794bbbf3f5b406
Mike <mikemcguiness@protonmail.com>
2023-10-23 19:56:39 -0700

    add keys to sub elements

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,8 +6,8 @@ const projectData = [
     title: 'Recreating React',
     subtitle: 'Building Four Core Features of React',
     source: 'https://github.com/mike-v2/react-from-scratch',
-    summary: <span>In this self-driven project, I undertook the challenge of recreating a simplified version of React from scratch to deepen my understanding of its inner workings. By building out core functions like createElement, render, useState, and useEffect, I gained valuable insights into JSX transformation, DOM manipulation, and state management. This hands-on experience has not only solidified my grasp of React fundamentals but also honed my problem-solving skills. I've documented my journey and the technical intricacies of the project in a <a target='blank' href="https://medium.com/@mmcguiness8700/recreating-react-96e8a01d4cb5" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>Medium article</a> and a <a target='blank' href="https://youtu.be/BU37lgD28lU" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>YouTube video</a> for those interested in diving deeper.</span>,
-    features: [<p><span className='font-bold'>createElement</span> transforms JSX into ReactElement objects</p>, <p><span className='font-bold'>render</span> brings ReactElements to life on the DOM</p>, <p><span className='font-bold'>useState</span> manages state</p>, <p><span className='font-bold'>useEffect</span> synchronizes the component with an external system</p>],
+    summary: <span>In this self-driven project, I undertook the challenge of recreating a simplified version of React from scratch to deepen my understanding of its inner workings.By building out core functions like createElement, render, useState, and useEffect, I gained valuable insights into JSX transformation, DOM manipulation, and state management.This hands-on experience has not only solidified my grasp of React fundamentals but also honed my problem-solving skills.I&apos;ve documented my journey and the technical intricacies of the project in a <a target='blank' href="https://medium.com/@mmcguiness8700/recreating-react-96e8a01d4cb5" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>Medium article</a> and a <a target='blank' href="https://youtu.be/BU37lgD28lU" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>YouTube video</a> for those interested in diving deeper.</span>,
+    features: [<p key={0}><span className='font-bold'>createElement</span> transforms JSX into ReactElement objects</p>, <p key={1}><span className='font-bold'>render</span> brings ReactElements to life on the DOM</p>, <p key={2}><span className='font-bold'>useState</span> manages state</p>, <p key={3}><span className='font-bold'>useEffect</span> synchronizes the component with an external system</p>],
     primaryTools: ['TypeScript', 'Parcel'],
     secondaryTools: ['Jest', 'Prettier', 'GitHub']
   },
@@ -128,9 +128,9 @@ export default function Projects() {
                       <summary className="collapse-title text-xl">Main Features</summary>
                       <div className="collapse-content">
                         <ul className='list-disc ps-8'>
-                          {project.features && project.features.map((feature, i) => {
+                          {project.features && project.features.map((feature, j) => {
                             return (
-                              <li key={i}>{feature}</li>
+                              <li key={j}>{feature}</li>
                             )
                           })}
                         </ul>
