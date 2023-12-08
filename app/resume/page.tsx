091f815076776346bb0c086de24da04a459c9f93
Mike <mikemcguiness@protonmail.com>
2023-12-07 20:54:57 -0800

    Update skills section of resume Add concepts Fix Wording Add Mongo

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -185,11 +185,11 @@ export default function Resume() {
             TECHNICAL SKILLS
           </h2>
           <ul className='flex flex-col gap-y-2 pt-4'>
-            <li><span className='font-bold'>Front-End:</span> HTML, CSS, JavaScript, Typescript, React, Next.js, Tailwind CSS, Jest, Git</li>
-            <li><span className='font-bold'>Database Management:</span> Prisma, Supabase (PostgreSQL), Firebase</li>
+            <li><span className='font-bold'>Front End:</span> HTML, CSS, JavaScript, Typescript, React, Next.js, Tailwind CSS</li>
+            <li><span className='font-bold'>Back End:</span> Prisma, Supabase, Firebase, Mongoose/MongoDB</li>
             <li><span className='font-bold'>Cloud Services:</span> Google Cloud Storage, Google Cloud Run, Google Cloud Functions</li>
-            <li><span className='font-bold'>APIs:</span> OpenAI API, Google Maps API, Yelp API</li>
-            <li><span className='font-bold'>Others:</span> Python, Unity, C#, Blender</li>
+            <li><span className='font-bold'>Others:</span> Jest, Git, Python, C#</li>
+            <li><span className='font-bold'>Concepts:</span>Testing, Version Control, REST API, AJAX, AGILE, PostgreSQL, NoSQL</li>
           </ul>
         </div>
 
