7826465d17384cb0033dc84de69697a880908082
Mike McGuiness <mikemcguiness@protonmail.com>
2024-02-05 15:51:51 -0800

    Move navbar to components folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,25 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import NavBar from './navbar'
+import { Inter } from "next/font/google";
+import NavBar from "@/components/navbar";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Mike McGuiness',
-  description: 'Portfolio of Michael McGuiness, front-end web developer',
-}
+  title: "Mike McGuiness",
+  description: "Portfolio of Michael McGuiness, front-end web developer",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
         {children}
-        {/* <Footer /> */}
       </body>
     </html>
-  )
+  );
 }
