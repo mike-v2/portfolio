cf2d7897a6c9c5c6e4ee4e24389666cb2b1ac8a0
Mike McGuiness <mikemcguiness@protonmail.com>
2024-02-05 16:41:48 -0800

    Fix header alignment in resume open source section

--- app/resume/openSource.tsx
+++ app/resume/openSource.tsx
@@ -29,45 +29,41 @@ export default function OpenSource() {
   return (
     <div className="py-6">
       <h2 className="font-bold pb-4">OPEN SOURCE CONTRIBUTIONS</h2>
-      <div className="flex flex-col gap-y-4">
-        <div className="">
-          <div className="flex">
-            <h4 className="font-bold pb-2">
-              Flutter Packages Repository (Contributor)
-            </h4>
-            <div className="flex flex-wrap justify-end gap-x-4 text-sm">
-              {flutterPRs &&
-                flutterPRs.map((pr) => (
-                  <a
-                    key={pr.name}
-                    href={pr.href}
-                    className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-                  >
-                    {pr.name}
-                  </a>
-                ))}
-            </div>
-          </div>
-
-          <ul className="flex flex-col gap-y-2 list-disc ps-8">
-            <li>
-              Improved documentation and code quality by extracting code
-              snippets from READMEs into separate compilable files, ensuring
-              accurate syntax and usage.
-            </li>
-            <li>
-              Collaborated on a significant open-source project managed by
-              Google, demonstrating effective teamwork in a large-scale, diverse
-              environment, and contributed to a widely-used codebase.
-            </li>
-            <li>
-              Enhanced tool support by adding functionality for .java, .gradle,
-              .sh, and .m files, integrating 9 commits into the main branch (PR
-              #5567), improving language versatility and developer experience.
-            </li>
-          </ul>
+      <div className="flex w-full">
+        <h4 className="font-bold pb-2 flex-auto">
+          Flutter Packages Repository (Contributor)
+        </h4>
+        <div className="flex flex-wrap justify-end gap-x-4 text-sm flex-auto">
+          {flutterPRs &&
+            flutterPRs.map((pr) => (
+              <a
+                key={pr.name}
+                href={pr.href}
+                className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+              >
+                {pr.name}
+              </a>
+            ))}
         </div>
       </div>
+
+      <ul className="flex flex-col gap-y-2 list-disc ps-8">
+        <li>
+          Improved documentation and code quality by extracting code snippets
+          from READMEs into separate compilable files, ensuring accurate syntax
+          and usage.
+        </li>
+        <li>
+          Collaborated on a significant open-source project managed by Google,
+          demonstrating effective teamwork in a large-scale, diverse
+          environment, and contributed to a widely-used codebase.
+        </li>
+        <li>
+          Enhanced tool support by adding functionality for .java, .gradle, .sh,
+          and .m files, integrating 9 commits into the main branch (PR #5567),
+          improving language versatility and developer experience.
+        </li>
+      </ul>
     </div>
   );
 }
