5104ef791ff8d150cff9249fea636ea00e1aff7a
Mike McGuiness <mikemcguiness@protonmail.com>
2024-03-29 16:06:22 -0700

    Create HoverSwellMenu

---
+++ components/hoverSwellMenu.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function HoverSwellMenu({
+  items,
+}: {
+  items: HoverSwellMenuItem[];
+}) {
+  return (
+    <div className='flex h-[50rem] w-full flex-col divide-y-2 divide-black'>
+      {items.map((item) => (
+        <Link
+          href={`/projects?project=${encodeURIComponent(item.title)}`}
+          className='group relative basis-3/4 overflow-hidden transition-all duration-500 hover:basis-full'
+        >
+          <div className='absolute inset-0 scale-125 transition-all duration-[3000ms] ease-out group-hover:scale-100'>
+            <Image
+              src={item.imagePath}
+              alt={item.title}
+              fill
+              className='object-cover'
+            />
+          </div>
+          <div className='absolute inset-0 z-10 w-full bg-teal-100 transition-all duration-700 group-hover:translate-x-full'></div>
+          <div className='relative z-20 flex h-full flex-col items-center p-4 md:flex-row'>
+            <h3 className='basis-1/3 whitespace-nowrap px-12 text-[clamp(1rem,2vw,4rem)] text-teal-400'>
+              {item.title}
+            </h3>
+            <div className='relative basis-2/3 text-[clamp(1rem,2vw,4rem)]'>
+              <p className='font-medium transition-colors duration-500 group-hover:text-teal-400'>
+                {item.subtitle}
+              </p>
+              <div className='absolute top-full h-fit overflow-y-hidden'>
+                <div className='flex gap-x-4 text-teal-400'>
+                  <p className='translate-y-full transition-transform duration-700 group-hover:translate-y-0'>{`Project Details`}</p>
+                  <p className='translate-y-full transition-transform duration-1000 group-hover:translate-y-0'>{`->`}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
---
+++ types/hoverSwellMenuItem.ts
@@ -0,0 +1,5 @@
+type HoverSwellMenuItem = {
+  title: string;
+  subtitle: string;
+  imagePath: string;
+};
