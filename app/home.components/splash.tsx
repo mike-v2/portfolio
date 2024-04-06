8cde0490f2d7f4db9839e558eb7cf39cb27fa924
Mike McGuiness <mikemcguiness@protonmail.com>
2024-04-05 17:30:43 -0700

    Fix media links sometimes not visible on splash screen

--- app/home.components/splash.tsx
+++ app/home.components/splash.tsx
@@ -5,7 +5,7 @@ import MediaLinks from '@/app/home.components/mediaLinks';
 
 export default function Splash() {
   return (
-    <section className='relative flex items-center bg-black'>
+    <section className='relative'>
       <Image
         src='/images/background-spotlight-teal.jpg'
         alt='Image of spotlight'
@@ -18,7 +18,7 @@ export default function Splash() {
       <div className='absolute inset-x-0 bottom-1/2 translate-y-1/2'>
         <SplashText />
       </div>
-      <div className='absolute inset-x-0 bottom-8'>
+      <div className='absolute right-4 top-4 md:right-16 md:top-12'>
         <MediaLinks />
       </div>
     </section>
