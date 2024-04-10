14435e23296ed8161a121546e461ae1a5cb52b98
Mike McGuiness <mikemcguiness@protonmail.com>
2024-04-09 21:23:53 -0700

    Fix home button only text is clickable

--- components/homeButton.tsx
+++ components/homeButton.tsx
@@ -2,13 +2,11 @@ import Link from 'next/link';
 
 export default function HomeButton() {
   return (
-    <div className='fixed left-2 top-8 z-30 border border-primary bg-base-300 px-8 py-4'>
-      <Link
-        href='/'
-        className='flex h-full items-center justify-center text-primary'
-      >
-        HOME
-      </Link>
-    </div>
+    <Link
+      href='/'
+      className='fixed left-2 top-8 z-30 flex items-center justify-center border border-primary bg-base-300 px-8 py-4 text-primary'
+    >
+      HOME
+    </Link>
   );
 }
