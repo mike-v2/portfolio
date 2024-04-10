23d37ce3c00e56d676b97a5a3bae53447d81afe8
Mike McGuiness <mikemcguiness@protonmail.com>
2024-04-09 21:21:09 -0700

    Improve responsiveness of blog page

--- app/blog/components/post.tsx
+++ app/blog/components/post.tsx
@@ -9,14 +9,14 @@ const catamaran = Catamaran({ subsets: ['latin'] });
 
 export default function Post({ title, date, link, content }: BlogPost) {
   return (
-    <section className='bg-base-300 p-8'>
+    <section className='bg-base-300 p-2 md:p-8'>
       <div className='mx-auto max-w-2xl py-16 text-center text-primary'>
-        <h3 className='my-12 text-8xl'>{title}</h3>
-        <div className='text-lg'>{format(date, 'MMM d, yyyy')}</div>
+        <h3 className='my-12 text-3xl md:text-8xl'>{title}</h3>
+        <div className='text-sm md:text-lg'>{format(date, 'MMM d, yyyy')}</div>
       </div>
 
       <div
-        className={`${catamaran.className} mx-auto mb-12 flex max-w-4xl flex-col gap-y-6 bg-base-100 p-24`}
+        className={`${catamaran.className} mx-auto mb-12 flex max-w-4xl flex-col gap-y-6 bg-base-100 p-4 md:p-24`}
       >
         <Anchor href={link} className='w-fit'>
           View on LinkedIn
